bf74fbc879ec8f86dd8b1d2ff9a16fdbd495811b
shmezi <alexirving992@gmail.com>
2022-07-04 23:26:07 +0300

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.0")
     implementation("org.litote.kmongo:kmongo:4.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.7")
+    implementation("com.google.code.gson:gson:2.9.0")
 }
 
 
