834db5f9b2373a567663d784f9b5d87a5b73f669
shmezi <alexirving992@gmail.com>
2022-07-30 00:11:50 +0300

    .

--- src/main/kotlin/me/alexirving/lib/Utils.kt
+++ src/main/kotlin/me/alexirving/lib/Utils.kt
@@ -86,3 +86,30 @@ fun <T> T?.pq(prefix: String?): T? {
         c = 0
     return this
 }
+
+enum class RomanNumerals(val amount: Int) {
+    M(1000),
+    CM(900),
+    D(500),
+    CD(400),
+    C(100),
+    XC(90),
+    L(50),
+    XL(40),
+    X(10),
+    IX(9),
+    V(5),
+    IV(4),
+    I(1)
+}
+
+fun Int.toRoman(): String {
+    var current = this
+    val appendable = StringBuilder()
+    for (r in RomanNumerals.values().filter { it.amount <= current }) {
+        val remove = current.floorDiv(r.amount)
+        appendable.append(r.name.repeat(remove))
+        current -= (r.amount * remove)
+    }
+    return appendable.toString()
+}
