7a4f2156241ff01b833210f56b81dcef29f0ba1c
shmezi <alexirving992@gmail.com>
2022-08-26 08:59:11 +0300

    Fixed MongoUtils.defaultClient

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.0")
-    implementation("org.litote.kmongo:kmongo:4.5.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+    implementation("org.litote.kmongo:kmongo:4.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.7")
     implementation("com.google.code.gson:gson:2.9.0")
     testImplementation(kotlin("test"))}
