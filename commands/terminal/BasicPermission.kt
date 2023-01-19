de6fd72d8c8009237cf09312304717df32f8ff1c
Tofaa <82680183+Tofaa2@users.noreply.github.com>
2023-01-19 19:21:43 +0300

    remove bs

--- commands/terminal/BasicPermission.kt
+++
@@ -1,8 +0,0 @@
-package me.alexirving.lib.commands.terminal
-
-import me.alexirving.lib.commands.Permission
-import java.util.*
-
-class BasicPermission : Permission<UUID> {
-    override fun hasPermission(user: UUID) = true
-}
